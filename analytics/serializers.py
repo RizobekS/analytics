49499e91e2c971ebecf9b2429fbd22f360f4d015
RizobekS <rizobeksaibov@gmail.com>
2025-09-17 16:10:37 +0500

    added workbook fullname id sheet_name and sheet_index for DatasetSerializer It helps for our frontend

--- analytics/serializers.py
+++ analytics/serializers.py
@@ -4,9 +4,16 @@ from .models import ChartConfig, Dashboard
 from ingest.models import Dataset, DatasetRow
 
 class DatasetSerializer(serializers.ModelSerializer):
+    fullname = serializers.CharField(source='sheet.workbook.filename', read_only=True)
+    workbook_id = serializers.IntegerField(source='sheet.workbook_id', read_only=True)
+    sheet_name = serializers.CharField(source='sheet.name', read_only=True)
+    sheet_index = serializers.IntegerField(source='sheet.index', read_only=True)
     class Meta:
         model = Dataset
-        fields = ["id", "name", "sheet_id", "inferred_schema", "meta"]
+        fields = [
+            "id", "name", "sheet_id", "inferred_schema", "meta",
+            "fullname", "workbook_id","sheet_name", "sheet_index",
+        ]
 
 class DatasetRowSerializer(serializers.ModelSerializer):
     class Meta:
--- analytics/views.py
+++ analytics/views.py
@@ -6,18 +6,25 @@ from .serializers import DatasetSerializer, DatasetRowSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from django.db.models.fields.json import KeyTextTransform
 
+
 class DatasetViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = DatasetSerializer
+
     def get_queryset(self):
         qs = Dataset.objects.all().annotate(n=Count('rows'))  # <-- FIX
         if self.request.query_params.get("only_nonempty"):
             qs = qs.filter(n__gt=0)
-        return qs.order_by("id")
+        return (qs
+                .select_related('sheet__workbook')
+                .order_by('id')
+                )
+
 
 class DatasetRowViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = DatasetRowSerializer
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     ordering = ["id"]
+
     def get_queryset(self):
         ds_id = self.kwargs["dataset_id"]
         qs = DatasetRow.objects.filter(dataset_id=ds_id)
